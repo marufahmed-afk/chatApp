062418bb388fc19ad14a752b7e0cb4309b9ad41c
Maruf Ahmed <maruf_m1@yahoo.com>
2020-07-08 14:17:32 +0600

    heroku test

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/chat", require("./routes/chat/chat"));
 app.use("/api/room", require("./routes/room/room"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
